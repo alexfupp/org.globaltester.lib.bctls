936f21afcb5942aa92349598d1209eef8821855d
Martin Boonk <martin.boonk@secunet.com>
2018-01-05 16:00:20 +0100

    Update bouncycastle to 1.59

---
+++ org.globaltester.lib.bctls/src/org/globaltester/lib/bctls/TlsCertificateGenerator.java
@@ -0,0 +1,75 @@
+package org.globaltester.lib.bctls;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
+import java.time.LocalDate;
+import java.time.ZoneOffset;
+import java.util.Calendar;
+import java.util.Date;
+
+import org.bouncycastle.asn1.x500.X500Name;
+import org.bouncycastle.asn1.x500.X500NameBuilder;
+import org.bouncycastle.asn1.x500.style.BCStyle;
+import org.bouncycastle.asn1.x509.BasicConstraints;
+import org.bouncycastle.asn1.x509.ExtendedKeyUsage;
+import org.bouncycastle.asn1.x509.Extension;
+import org.bouncycastle.asn1.x509.KeyPurposeId;
+import org.bouncycastle.asn1.x509.KeyUsage;
+import org.bouncycastle.cert.X509CertificateHolder;
+import org.bouncycastle.cert.X509v3CertificateBuilder;
+import org.bouncycastle.cert.jcajce.JcaX509CertificateConverter;
+import org.bouncycastle.cert.jcajce.JcaX509v3CertificateBuilder;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.operator.ContentSigner;
+import org.bouncycastle.operator.OperatorCreationException;
+import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder;
+
+public class TlsCertificateGenerator {
+
+	private static SecureRandom random = new SecureRandom();
+
+	public static KeyPair generateKeyPair() throws NoSuchAlgorithmException {
+		// create keypair
+		KeyPairGenerator keypairGen = KeyPairGenerator.getInstance("RSA");
+		keypairGen.initialize(2048, random);
+		KeyPair keypair = keypairGen.generateKeyPair();
+		return keypair;
+	}
+
+	public static Certificate generateTlsCertificate(KeyPair keypair) {
+		try {
+			X500Name subject = new X500NameBuilder(BCStyle.INSTANCE).addRDN(BCStyle.CN, "PersoSim").build();
+			byte[] id = new byte[16];
+			random.nextBytes(id);
+			BigInteger serial = new BigInteger(160, random);
+			X509v3CertificateBuilder certificate = new JcaX509v3CertificateBuilder(subject, serial,
+					Calendar.getInstance().getTime(),
+					Date.from(LocalDate.of(2030, 1, 1).atStartOfDay(ZoneOffset.UTC).toInstant()), subject,
+					keypair.getPublic());
+			certificate.addExtension(Extension.subjectKeyIdentifier, false, id);
+			certificate.addExtension(Extension.authorityKeyIdentifier, false, id);
+			BasicConstraints constraints = new BasicConstraints(true);
+			certificate.addExtension(Extension.basicConstraints, true, constraints.getEncoded());
+			KeyUsage usage = new KeyUsage(KeyUsage.keyCertSign | KeyUsage.digitalSignature);
+			certificate.addExtension(Extension.keyUsage, false, usage.getEncoded());
+			ExtendedKeyUsage usageEx = new ExtendedKeyUsage(
+					new KeyPurposeId[] { KeyPurposeId.id_kp_serverAuth, KeyPurposeId.id_kp_clientAuth });
+			certificate.addExtension(Extension.extendedKeyUsage, false, usageEx.getEncoded());
+
+			ContentSigner signer = new JcaContentSignerBuilder("SHA256withRSA").build(keypair.getPrivate());
+			X509CertificateHolder holder = certificate.build(signer);
+
+			JcaX509CertificateConverter converter = new JcaX509CertificateConverter();
+			converter.setProvider(new BouncyCastleProvider());
+			return converter.getCertificate(holder);
+		} catch (CertificateException | OperatorCreationException | IOException e) {
+			throw new IllegalStateException("Could not create self signed certificate", e);
+		}
+	}
+}
